220b566dcb1009bcc3076874a1554b155b8e29ae
ychen <55662670+YuChenSSR@users.noreply.github.com>
2024-06-25 17:41:45 +0800

    Update azure_tts.sh

--- azure_tts.sh
+++ azure_tts.sh
@@ -1,30 +1,27 @@
 #!/bin/zsh
 # #popclip
-# name: Azure TTS
+# name: openai TTS
 # icon: symbol:message.and.waveform
 # options:
-# - { identifier: region, label: Azure Region, type: string, default value: eastus, description: "Region for Speech Service" }
-# - { identifier: key, label: Azure Subscription Key, type: string, description: "The key of Speech Resource" }
-# - { identifier: format, label: Audio Output, type: string, default value: audio-16khz-32kbitrate-mono-mp3, description: "Microsoft Output Format" }
-# - { identifier: lang, label: Locale, type: string, default value: en-US, description: "The language to speak" }
-# - { identifier: voice, label: Text-to-speech Voice, type: string, default value: en-US-JennyMultilingualNeural, description: "The voice name with locale prefixed" }
+# - { identifier: url, label: BASE_URL, type: string, default value: https://api.oaifree.com/v1/audio/speech, description: "openai API URL for Speech Service" }
+# - { identifier: api_key, label: OpenAI API Key, type: string, description: "The API key for OpenAI" }
 
-# Create a temporary audio file
-temp_audio_file=$(mktemp)
+# Create a temporary variable for input
+input_content="$POPCLIP_TEXT"
 
 # Use curl to download and save the audio data to the temporary file
-curl -X POST "https://${POPCLIP_OPTION_REGION}.tts.speech.microsoft.com/cognitiveservices/v1" \
-     -H "Ocp-Apim-Subscription-Key: ${POPCLIP_OPTION_KEY}" \
-     -H "Content-Type: application/ssml+xml" \
-     -H "X-Microsoft-OutputFormat: ${POPCLIP_OPTION_FORMAT}" \
-     -d "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"${POPCLIP_OPTION_LANG}\">
-    <voice name=\"${POPCLIP_OPTION_VOICE}\">
-        $POPCLIP_TEXT
-    </voice>
-</speak>" -so "$temp_audio_file"
+curl "${POPCLIP_OPTION_URL}" \
+  -H "Authorization: Bearer ${POPCLIP_OPTION_API_KEY}" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "tts-1",
+    "input": "'"${input_content//\"/\\\"}"'",
+    "voice": "shimmer"
+  }' \
+  --output speech.mp3
 
 # Play the temporary audio file using afplay
-afplay "$temp_audio_file"
+afplay speech.mp3
 
 # Clean up the temporary audio file when you're done with it
-rm "$temp_audio_file"
+rm speech.mp3
